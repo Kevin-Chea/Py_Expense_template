ad1696aafae45e39fcb79ee5905bdbb466610a75
Kevin Chea <kevin.chea@epita.fr>
2022-11-02 19:42:20 +0100

    [ADD][FEATURE] Add a new expense works

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -23,6 +24,20 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    # We read the file. If an expense with the same label exists, the new one is not registered.
+    # Each line has a label, an amount and the spender (in this order)
+    with open('expense_report.csv', 'r', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            if row[0] == infos["label"]:
+                print("An expense with this label already exists.")
+                return False
+    # Add the new expense
+    with open('expense_report.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([infos["label"], infos["amount"], infos["spender"]])
+    
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
